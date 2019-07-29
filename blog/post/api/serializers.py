b6b23d466f349db8c8d9c10036bc6ac1400b057d
selametsamli <selamet96@gmail.com>
2019-07-30 00:10:07 +0300

    Post modele eklemeler yapıldı

--- blog/post/api/serializers.py
+++ blog/post/api/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 
+from post.models import Post
 
-class PostSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=200)
-    content = serializers.CharField(max_length=200)
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'image', 'slug', 'created']
--- blog/post/models.py
+++ blog/post/models.py
@@ -1,10 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils import timezone
 
 # Create your models here.
+from django.utils.text import slugify
+
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     title = models.CharField(max_length=120)
     content = models.TextField()
+    draft = models.BooleanField(default=False)
+    created = models.DateTimeField(editable=False)
+    modified = models.DateTimeField()
+    slug = models.SlugField(unique=True, max_length=150, editable=False)
+    image = models.ImageField(upload_to='media/post/')
+
+    def get_slug(self):
+        slug = slugify(self.title.replace("ı", "i"))
+        unique = slug
+        number = 1
+
+        while Post.objects.filter(slug=unique).exists():
+            unique = '{}-{}'.format(slug, number)
+            number += 1
+        return unique
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created = timezone.now()
+
+        self.modified = timezone.now()
+        self.slug = self.get_slug()
+        return super(Post, self).save(*args, **kwargs)
