9fe26499cbab69ea737778d0e85bd5165de87d5d
selametsamli <selamet96@gmail.com>
2019-08-03 18:53:09 +0300

    Caching işlemi yapıldı

--- blog/post/api/urls.py
+++ blog/post/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
+from django.views.decorators.cache import cache_page
 
 from post.api.views import PostListAPIView, PostDetailAPIView, PostUpdateAPIView, PostCreateAPIView
 
 app_name = 'post'
 
 urlpatterns = [
-    path('list', PostListAPIView.as_view(), name='list'),
+    path('list', cache_page(60 * 1)(PostListAPIView.as_view()), name='list'),
     path('detail/<slug>', PostDetailAPIView.as_view(), name='detail'),
     path('update/<slug>', PostUpdateAPIView.as_view(), name='update'),
     path('create', PostCreateAPIView.as_view(), name='create'),
